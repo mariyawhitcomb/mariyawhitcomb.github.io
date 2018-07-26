c60cbac56f038bb1a3d6d915f7f3f84adb8e8a03
Mariya <mariyawhitcomb@gmail.com>
2018-07-26 16:12:44 -0400

    changed background styling

--- script.js
+++ script.js
@@ -18,20 +18,3 @@ jQuery(function($) {
   $(window).scroll(fixDiv);
   fixDiv();
 });
-// const projects = document.querySelectorAll(".project");
-
-// function toggleOpen() {
-//   this.classList.toggle("open");
-// }
-
-// function toggleActive(e) {
-//   console.log(e.propertyName);
-//   if (e.propertyName.includes("flex")) {
-//     this.classList.toggle("open-active");
-//   }
-// }
-
-// projects.forEach(project => project.addEventListener("click", toggleOpen));
-// projects.forEach(project =>
-//   project.addEventListener("transitionend", toggleActive)
-// );
